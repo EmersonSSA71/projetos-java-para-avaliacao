8d00b1ba470072d06606b48da636776ed2464efc
EmersonSSA71 <emersocial.mec@gmail.com>
2026-06-18 13:15:39 -0300

    Correcao da tarefa realizada em 17-06-2026

--- avancado-laco-e-controle-de-fluxo/src/LacoEControleDeFluxo.java
+++ avancado-laco-e-controle-de-fluxo/src/LacoEControleDeFluxo.java
@@ -3,149 +3,155 @@ import java.util.Scanner;
 public class LacoEControleDeFluxo {
 
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
+
+        Tabuada(sc);
+        Menu(sc);
+
+        sc.close();
+    }
+
+
+    public static void Tabuada (Scanner sc) {
+
         String OpcaoEscolhida;
         String NumeroEscolhido1;
         String NumeroEscolhido2;
         String NumeroEscolhido3;
         String NumeroEscolhido4;
-        String EscolhaDoUsuario;
-        int Fator = 0;
+        int Fator;
 
         do {
-            System.out.println("\nTABUADA SIMPLES\nDigite a opção de qual tipo de tabuada:\n1. Soma\n2. Subtração\n3. Multiplicação\n4. Divisão\n00. Pular Etapa");
-            OpcaoEscolhida = sc.next();
+            System.out.println("\nTABUADA SIMPLES\nDigite a opção de qual tipo de tabuada:\n1. Soma\n2. Subtração\n3. Multiplicação\n4. Divisão\n0. Pular Etapa");
+            OpcaoEscolhida = sc.nextLine();
+
+            Fator = 0;
+
+            if (OpcaoEscolhida.equals("1")) {
 
                 // SOMA //
 
-            if (OpcaoEscolhida.equals("1")) {
-                    do {
-                        System.out.println("Digite um numero 0 a 10 para calcular sua tabuada (A qualquer momento digite '00' para pular etapa):");
-                        NumeroEscolhido1 = sc.next();
+                do {
+                    System.out.println("Digite um numero 0 a 10 para calcular sua tabuada:");
+                    NumeroEscolhido1 = sc.nextLine();
 
-                        if (!NumeroEscolhido1.matches("[0-9]|10")) {
-                            System.out.println("!Erro!");
-                        }
-                    } while (!NumeroEscolhido1.matches("[0-9]|10"));
+                    if (!NumeroEscolhido1.matches("[0-9]|10")) {
+                        System.out.println("!Erro!");
+                    }
+                } while (!NumeroEscolhido1.matches("[0-9]|10"));
 
-                    int NumeroInteiro1 = Integer.parseInt(NumeroEscolhido1);
+                int NumeroInteiro1 = Integer.parseInt(NumeroEscolhido1);
 
-                    while (Fator <= 10) {
-                        System.out.println(NumeroInteiro1 + " + " + Fator + " = " + (NumeroInteiro1 + Fator));
+                while (Fator <= 10) {
+                    System.out.println(NumeroInteiro1 + " + " + Fator + " = " + (NumeroInteiro1 + Fator));
+                    Fator++;
+                }
+
+                // SUBTRACÃO //
+
+            } else if (OpcaoEscolhida.equals("2")) {
+                do {
+                    System.out.println("Digite um numero 0 a 10 para calcular sua tabuada:");
+                    NumeroEscolhido2 = sc.nextLine();
+                    if (!NumeroEscolhido2.matches("[0-9]|10")) {
+                        System.out.println("!Erro!");
+                    }
+                } while (!NumeroEscolhido2.matches("[0-9]|10"));
+
+                int NumeroInteiro2 = Integer.parseInt(NumeroEscolhido2);
+
+                while (Fator <= 10)
+                    if (NumeroInteiro2 >= Fator) {
+                        System.out.println(NumeroInteiro2 + " - " + Fator + " = " + Math.abs(NumeroInteiro2 - Fator));
+                        Fator++;
+                    } else {
+                        System.out.println(NumeroInteiro2 + " - " + Fator + " = " + (NumeroInteiro2 - Fator));
                         Fator++;
                     }
 
-                    // SUBTRACÃO //
+                // MULTIPLICAÇÃO //
 
-                } else if (OpcaoEscolhida.equals("2")) {
-                    do {
-                        System.out.println("Digite um numero 0 a 10 para calcular sua tabuada (Ou qualquer momento digite '00' pular etapa):");
-                        NumeroEscolhido2 = sc.next();
-                        if (!NumeroEscolhido2.matches("[0-9]|10")) {
-                            System.out.println("!Erro!");
-                        }
-                    } while (!NumeroEscolhido2.matches("[0-9]|10"));
+            } else if (OpcaoEscolhida.equals("3")) {
+                do {
+                    System.out.println("Digite um numero 0 a 10 para calcular sua tabuada:");
+                    NumeroEscolhido3 = sc.nextLine();
+                    if (!NumeroEscolhido3.matches("[0-9]|10")) {
+                        System.out.println("!Erro!");
+                    }
+                } while (!NumeroEscolhido3.matches("[0-9]|10"));
 
-                    int NumeroInteiro2 = Integer.parseInt(NumeroEscolhido2);
+                int NumeroInteiro3 = Integer.parseInt(NumeroEscolhido3);
 
-                    while (Fator <= 10)
-                        if (NumeroInteiro2 >= Fator) {
-                            System.out.println(NumeroInteiro2 + " - " + Fator + " = " + Math.abs(NumeroInteiro2 - Fator));
-                            Fator++;
-                        } else {
-                            System.out.println(NumeroInteiro2 + " - " + Fator + " = " + (NumeroInteiro2 - Fator));
-                            Fator++;
-                        }
+                while (Fator <= 10) {
+                    System.out.println(NumeroEscolhido3 + " x " + Fator + " = " + (NumeroInteiro3 * Fator));
+                    Fator++;
+                }
 
-                    // MULTIPLICAÇÃO //
+                // DIVISÃO //
 
-                } else if (OpcaoEscolhida.equals("3")) {
-                    do {
-                        System.out.println("Digite um numero 0 a 10 para calcular sua tabuada (A qualquer momento digite '00' para pular etapa):");
-                        NumeroEscolhido3 = sc.next();
-                        if (!NumeroEscolhido3.matches("[0-9]|10")) {
-                            System.out.println("!Erro!");
-                        }
-                    } while (!NumeroEscolhido3.matches("[0-9]|10"));
+            } else if (OpcaoEscolhida.equals("4")) {
+                do {
+                    System.out.println("Digite um numero entre 0 a 10 para calcular sua tabuada:");
+                    NumeroEscolhido4 = sc.nextLine();
+                    if (!NumeroEscolhido4.matches("[0-9]|10")) {
+                        System.out.println("!Erro!");
+                    }
+                } while (!NumeroEscolhido4.matches("[0-9]|10"));
 
-                    int NumeroInteiro3 = Integer.parseInt(NumeroEscolhido3);
+                double NumeroInteiro4 = Double.parseDouble(NumeroEscolhido4);
 
-                    while (Fator <= 10) {
-                        System.out.println(NumeroEscolhido3 + " x " + Fator + " = " + (NumeroInteiro3 * Fator));
+                while (Fator <= 10) {
+                    if (NumeroInteiro4 == 0) {
+                        System.out.println(NumeroInteiro4 + " / " + Fator + " = " + (0));
+                        Fator++;
+                    } else if (Fator == 0) {
+                        System.out.println(NumeroInteiro4 + " / " + Fator + " = " + (0));
+                        Fator++;
+                    } else {
+                        System.out.println(NumeroInteiro4 + " / " + Fator + " = " + (NumeroInteiro4 / Fator));
                         Fator++;
                     }
+                }
 
-                    // DIVISÃO //
+                // SAÍDA //
 
-                } else if (OpcaoEscolhida.equals("4")) {
+            } else if (OpcaoEscolhida.equals("0")) {
+                break;
+            } else {
+                System.out.println("\n!Erro!\nDigite uma opção válida!");
+            }
+        } while (true);
+    }
+
+        public static void Menu (Scanner sc) {
+
+        String EscolhaDoUsuario2;
+        String PalavraOuFraseDoUsuario;
+
+            do {
+                System.out.println("Escolha o que gostaria de fazer agora:\n1. Continuar\n2. Sair");
+                EscolhaDoUsuario2 = sc.nextLine();
+
+                if (EscolhaDoUsuario2.equals("1")) {
                     do {
-                        System.out.println("Digite um numero entre 0 a 10 para calcular sua tabuada (A qualquer momento digite '00' para pular etapa):");
-                        NumeroEscolhido4 = sc.next();
-                        if (!NumeroEscolhido4.matches("[0-9]|10")) {
-                            System.out.println("!Erro!");
-                        }
-                    } while (!NumeroEscolhido4.matches("[0-9]|10"));
+                        System.out.println("\nDigite uma palavra ou uma frase:");
+                        PalavraOuFraseDoUsuario = sc.nextLine();
 
-                    double NumeroInteiro4 = Double.parseDouble(NumeroEscolhido4);
-
-                    while (Fator <= 10) {
-                        if (NumeroInteiro4 == 0) {
-                            System.out.println(NumeroInteiro4 + " / " + Fator + " = " + (0));
-                            Fator++;
-                        } else if (Fator == 0) {
-                            System.out.println(NumeroInteiro4 + " / " + Fator + " = " + (0));
-                            Fator++;
+                        if (!PalavraOuFraseDoUsuario.matches("[a-zA-Z ]+")) {
+                            System.out.println("\n!Erro!\nDigite uma palavra ou frase válida!\n");
                         } else {
-                            System.out.println(NumeroInteiro4 + " / " + Fator + " = " + (NumeroInteiro4 / Fator));
-                            Fator++;
+                            System.out.println("Essa foi a palavra ou frase que você digitou: > " + PalavraOuFraseDoUsuario + " <\n");
+                            break;
                         }
-                    }
+                    } while (!PalavraOuFraseDoUsuario.matches("[a-zA-Z ]+"));
 
-                    // SAÍDA //
 
-                } else if (OpcaoEscolhida.equals("00")) {
+                } else if (EscolhaDoUsuario2.equals("2")) {
+                    System.out.println("\nObrigado por utilizar o nosso sistema, espero que tenha gostado.");
                     break;
                 } else {
-                    System.out.println("\n!Erro!\nDigite uma opção válida!");
+                    System.out.println("\n!Erro!\nDigite uma opção válida!\n");
                 }
-        } while (true);
-
-        do {
-            System.out.println("Escolha oque gostaria de fazer agora:\n1. Continuar\n2. Sair");
-            EscolhaDoUsuario = sc.next();
-
-            if (EscolhaDoUsuario.equals("1")) {
-                Continuar();
-            } else if (EscolhaDoUsuario.equals("2")) {
-                Sair();
-            } else {
-                System.out.println("\n!Erro!\nDigite uma opção válida!\n");
-                EscolhaDoUsuario = ("3");
-            }
-        } while (EscolhaDoUsuario.equals("3"));
-
-        sc.close();
-    }
-
-    public static void Continuar() {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Digite uma palavra ou uma frase:");
-        String PalavraOuFraseDoUsuario;
-        do {
-            PalavraOuFraseDoUsuario = sc.nextLine();
-            if (!PalavraOuFraseDoUsuario.matches("[a-zA-Z ]+")) {
-                System.out.println("\n!Erro!\nDigite uma palavra ou frase válida!\n");
-            }
-        } while (!PalavraOuFraseDoUsuario.matches("[a-zA-Z ]+"));
-        System.out.println("Essa foi a palavra ou frase que você digitou: > " + PalavraOuFraseDoUsuario + " <");
-        sc.close();
-        Sair();
-
-    }
-
-    public static void Sair() {
-        System.out.println("\nObrigado por utilizar o nosso sistema, espero que tenha gostado.");
-
+            } while (true);
     }
 }
