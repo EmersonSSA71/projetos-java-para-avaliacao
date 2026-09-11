da3d8f3add6159ca921685ff9ff82ee25be7454c
EmersonSSA71 <emersocial.mec@gmail.com>
2026-09-11 15:03:25 -0300

    Subindo correção no catch mudança: RuntimeException(e) para System.out.println(e.getMessage()); para ficar mais agradavel.

--- exceptions-try-catch/src/CadastroUsuario.java
+++ exceptions-try-catch/src/CadastroUsuario.java
@@ -4,14 +4,14 @@ public class CadastroUsuario {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Digite seu nome: ");
-        String nome = scanner.next();
+        String nome = scanner.nextLine();
         System.out.print("Digite sua idade: ");
         int idade = scanner.nextInt();
 
         try {
             cadastrarUsuario(nome, idade);
         } catch (IdadeInvalidaException e) {
-            throw new RuntimeException(e);
+            System.out.println(e.getMessage());
         } finally {
             System.out.print("Processo de cadastro finalizado.");
         }
@@ -22,7 +22,7 @@ public class CadastroUsuario {
         if (idade < 18) {
             throw new IdadeInvalidaException("Idade inválida! Usuario precisa ter 18 anos ou mais");
         }
-        else if (idade > 18) {
+        else {
             System.out.println("Usuário " + nome + ", foi cadastrado com sucesso.");
         }
 
