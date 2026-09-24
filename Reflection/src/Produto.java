a3aa064a84beb5100e1e13a4124d4d098663bfc1
EmersonSSA71 <emersocial.mec@gmail.com>
2026-09-24 09:24:23 -0300

    Subindo novo assunto pratica java

---
+++ Reflection/src/Produto.java
@@ -0,0 +1,20 @@
+public class Produto {
+    private int valor = 200;
+    private String nome;
+
+    public int getValor() {
+        return valor;
+    }
+
+    public void setValor(int valor) {
+        this.valor = valor;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
---
+++ Reflection/src/Reflection.java
@@ -0,0 +1,19 @@
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
+public class Reflection {
+    public static void main(String... args) throws Exception {
+
+        Produto p = new Produto();
+
+        Class c = Produto.class;
+        Field f;
+        Method m;
+
+
+    }
+
+    private static void testar(Object obj){
+
+    }
+}
